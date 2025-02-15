798e15a715c21f47c6676c0658dc5aa3864d9418
XavierZXY <zxy_xavier@163.com>
2025-02-15 18:29:10 +0800

    partial update transformerxl

--- Transformers_Learn/transformerXL.py
+++ Transformers_Learn/transformerXL.py
@@ -15,3 +15,37 @@ from .relative_mha import RelativeMultiHeadAttention
 # and feed them to current step. If we use fixed-positional encodings these pre-calculated embeddings will have the
 # same positions as the current context. They introduce relative positional encoding, where the positional encodings
 # are introduced at the attention calculation.
+
+
+class TransformerXLLayer(nn.Module):
+    def __init__(
+        self,
+        *,
+        d_model: int,
+        self_attn: RelativeMultiHeadAttention,
+        feed_forward: FeedForward,
+        dropout_prob: float,
+    ):
+        super().__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.dropout = nn.Dropout(dropout_prob)
+        self.norm_self_attn = nn.LayerNorm([d_model])
+        self.norm_ff = nn.LayerNorm([d_model])
+
+    def forward(
+        self, x: torch.Tensor, mem: Optional[torch.Tensor], mask: torch.Tensor
+    ):
+        z = self.norm_self_attn(x)
+        if mem is not None:
+            mem = self.norm_self_attn(mem)
+            m_z = torch.cat([mem, z], dim=0)
+        else:
+            m_z = z
+
+        self_attn = self.self_attn(z, m_z, m_z, mask)
+        x = x + self.dropout(self_attn)
+        z = self.norm_ff(x)
+        ff = self.feed_forward(z)
+        x = x + self.dropout(ff)
+        return x
